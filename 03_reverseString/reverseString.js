d3c9f8782e6616de2642b8cdd3238228f9b1db2a
besplago <bruce_esplago@hotmail.com>
2025-03-19 21:11:58 +0100

    solved: 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    if (string.length <= 1) {
+        return string
+    }
+    return string[string.length - 1] + reverseString(string.slice(0, string.length - 1))
 };
 
 // Do not edit below this line
