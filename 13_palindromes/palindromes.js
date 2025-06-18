2da0c80bae9147d199711224777a75551b106403
besplago <bruce_esplago@hotmail.com>
2025-06-18 20:33:18 +0200

    solved: 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
-};
+const palindromes = function (word) {
+  // Removes spaces and punctuation marks
+  const cleanString = word
+    .replace(/[\s\p{P}]+/gu, match => (match === '_' ? '_' : ''))
+    .toLowerCase()
+  const reversedCleanString = cleanString.split('').reverse().join('')
+  return cleanString === reversedCleanString
+}
 
 // Do not edit below this line
-module.exports = palindromes;
+module.exports = palindromes
