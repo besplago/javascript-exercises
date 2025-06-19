bcb5a7c4cebdf7ad9f2eb1c4ad52e7427f3ef558
besplago <bruce_esplago@hotmail.com>
2025-06-19 12:10:42 +0200

    solved: 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n < 0) {
+    return 'OOPS'
+  }
 
-};
+  const fib = [0, 1]
+  for (let i = 2; i <= n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2]
+  }
+  return fib[n]
+}
 
 // Do not edit below this line
-module.exports = fibonacci;
+module.exports = fibonacci
