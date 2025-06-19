9ee78b61a6f4af94b38f4bbd954b5aa0afb12e59
besplago <bruce_esplago@hotmail.com>
2025-06-19 13:08:04 +0200

    solved: 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let oldestPersonIdx = 0
+  for (let personIdx = 0; personIdx < people.length; personIdx++) {
+    let personAYearOfDeath = people[oldestPersonIdx].yearOfDeath
+    let personBYearOfDeath = people[personIdx].yearOfDeath
 
-};
+    if (personAYearOfDeath === undefined) {
+      personAYearOfDeath = new Date().getFullYear()
+    }
+
+    if (personBYearOfDeath === undefined) {
+      personBYearOfDeath = new Date().getFullYear()
+    }
+
+    const personAgeA = personAYearOfDeath - people[oldestPersonIdx].yearOfBirth
+    const personAgeB = personBYearOfDeath - people[personIdx].yearOfBirth
+    if (personAgeA < personAgeB) {
+      oldestPersonIdx = personIdx
+    }
+  }
+  return people[oldestPersonIdx]
+}
 
 // Do not edit below this line
-module.exports = findTheOldest;
+module.exports = findTheOldest
